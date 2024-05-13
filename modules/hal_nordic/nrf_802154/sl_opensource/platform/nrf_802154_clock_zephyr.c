861f4d7b55c9e85f96ab3ea79b9ab82b6c29cae8
Piotr Koziar <piotr.koziar@nordicsemi.no>
2024-05-13 22:17:47 +0200

    Hotfix for hfclk rquesting - make a callout to RD.

--- modules/hal_nordic/nrf_802154/sl_opensource/platform/nrf_802154_clock_zephyr.c
+++ modules/hal_nordic/nrf_802154/sl_opensource/platform/nrf_802154_clock_zephyr.c
@@ -9,13 +9,49 @@
 #include <stddef.h>
 
 #include <compiler_abstraction.h>
+#if defined(CONFIG_CLOCK_CONTROL_NRF)
 #include <zephyr/drivers/clock_control/nrf_clock_control.h>
+#else
+#include "nrf_802154_config.h"
+#endif
 #include <zephyr/drivers/clock_control.h>
 
+#if defined(CONFIG_CLOCK_CONTROL_NRF)
 static bool hfclk_is_running;
-static bool lfclk_is_running;
 static struct onoff_client hfclk_cli;
-static struct onoff_client lfclk_cli;
+
+static void hfclk_on_callback(struct onoff_manager *mgr,
+			      struct onoff_client  *cli,
+			      uint32_t state,
+			      int res)
+{
+	hfclk_is_running = true;
+	nrf_802154_clock_hfclk_ready();
+}
+
+bool nrf_802154_clock_hfclk_is_running(void)
+{
+	return hfclk_is_running;
+}
+
+#else
+
+#define NRF_LRCCONF_RADIO_PD NRF_LRCCONF010
+
+static void hfclk_start(void)
+{	
+	/* Use register directly, there is no support for that task in nrf_lrcconf_task_trigger */
+	NRF_LRCCONF_RADIO_PD->EVENTS_HFXOSTARTED = 0x0;
+	NRF_LRCCONF_RADIO_PD->TASKS_REQHFXO = 0x1;
+}
+
+static void hfclk_stop(void)
+{
+	/* Use register directly, there is no support for that task in nrf_lrcconf_task_trigger */
+	NRF_LRCCONF_RADIO_PD->TASKS_STOPREQHFXO=0x1;
+	NRF_LRCCONF_RADIO_PD->EVENTS_HFXOSTARTED = 0x0;
+}
+#endif
 
 void nrf_802154_clock_init(void)
 {
@@ -27,17 +63,9 @@ void nrf_802154_clock_deinit(void)
 	/* Intentionally empty. */
 }
 
-static void hfclk_on_callback(struct onoff_manager *mgr,
-			      struct onoff_client  *cli,
-			      uint32_t state,
-			      int res)
-{
-	hfclk_is_running = true;
-	nrf_802154_clock_hfclk_ready();
-}
-
 void nrf_802154_clock_hfclk_start(void)
 {
+#if defined(CONFIG_CLOCK_CONTROL_NRF)
 	int ret;
 	struct onoff_manager *mgr =
 		z_nrf_clock_control_get_onoff(CLOCK_CONTROL_NRF_SUBSYS_HF);
@@ -48,10 +76,15 @@ void nrf_802154_clock_hfclk_start(void)
 
 	ret = onoff_request(mgr, &hfclk_cli);
 	__ASSERT_NO_MSG(ret >= 0);
+#else
+	hfclk_start();
+	nrf_802154_clock_hfclk_ready();
+#endif
 }
 
 void nrf_802154_clock_hfclk_stop(void)
 {
+#if defined(CONFIG_CLOCK_CONTROL_NRF)
 	int ret;
 	struct onoff_manager *mgr =
 		z_nrf_clock_control_get_onoff(CLOCK_CONTROL_NRF_SUBSYS_HF);
@@ -61,55 +94,8 @@ void nrf_802154_clock_hfclk_stop(void)
 	ret = onoff_cancel_or_release(mgr, &hfclk_cli);
 	__ASSERT_NO_MSG(ret >= 0);
 	hfclk_is_running = false;
+#else
+	hfclk_stop();
+#endif
 }
 
-bool nrf_802154_clock_hfclk_is_running(void)
-{
-	return hfclk_is_running;
-}
-
-static void lfclk_on_callback(struct onoff_manager *mgr,
-			      struct onoff_client  *cli,
-			      uint32_t state,
-			      int res)
-{
-	lfclk_is_running = true;
-	nrf_802154_clock_lfclk_ready();
-}
-
-void nrf_802154_clock_lfclk_start(void)
-{
-	int ret;
-	struct onoff_manager *mgr =
-		z_nrf_clock_control_get_onoff(CLOCK_CONTROL_NRF_SUBSYS_LF);
-
-	__ASSERT_NO_MSG(mgr != NULL);
-
-	sys_notify_init_callback(&lfclk_cli.notify, lfclk_on_callback);
-
-	ret = onoff_request(mgr, &lfclk_cli);
-	__ASSERT_NO_MSG(ret >= 0);
-}
-
-void nrf_802154_clock_lfclk_stop(void)
-{
-	int ret;
-	struct onoff_manager *mgr =
-		z_nrf_clock_control_get_onoff(CLOCK_CONTROL_NRF_SUBSYS_LF);
-
-	__ASSERT_NO_MSG(mgr != NULL);
-
-	ret = onoff_cancel_or_release(mgr, &lfclk_cli);
-	__ASSERT_NO_MSG(ret >= 0);
-	lfclk_is_running = false;
-}
-
-bool nrf_802154_clock_lfclk_is_running(void)
-{
-	return lfclk_is_running;
-}
-
-__WEAK void nrf_802154_clock_lfclk_ready(void)
-{
-	/* Intentionally empty. */
-}
